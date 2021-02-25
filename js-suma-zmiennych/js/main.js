4a7239ee4196c9135c4bfe69e4fe7fc1b473ad0d
jbadocha <jadwiga.badocha@gmail.com>
2021-02-25 21:07:01 +0100

    suma zmiennych

---
+++ js-suma-zmiennych/js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 8;
+let secondNumber = 2;
+
+function multiply(firstNumber, secondNumber){
+    let  result = firstNumber + secondNumber;
+    
+    return result;
+}
+let suma = multiply(firstNumber, secondNumber);
+
+console.log(suma);
