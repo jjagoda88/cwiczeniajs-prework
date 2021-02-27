1e24512e0519d6f31c8a67457d9062710f6063cc
jbadocha <jadwiga.badocha@gmail.com>
2021-02-27 16:34:30 +0100

    suma zmiennych poprawa

--- js-suma-zmiennych/js/main.js
+++ js-suma-zmiennych/js/main.js
@@ -1,11 +1,11 @@
 let firstNumber = 8;
 let secondNumber = 2;
 
-function multiply(firstNumber, secondNumber){
+function sum(firstNumber, secondNumber){
     let  result = firstNumber + secondNumber;
     
     return result;
 }
-let suma = multiply(firstNumber, secondNumber);
+let suma = sum(firstNumber, secondNumber);
 
 console.log(suma);
